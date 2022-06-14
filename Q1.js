24d7cdba63cdd01e4c89baa2d6e2637690e63763
owentr1369 <tamvaa1306@gmail.com>
2022-06-14 15:52:30 +0700

    max arguments

--- Q1.js
+++ Q1.js
@@ -1,9 +1,5 @@
 function customCalc() {
-  let sum = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    sum += arguments[i];
-  }
-  console.log(sum);
+  console.log(Math.max(...arguments));
 }
 
 customCalc(4, 7, 32, 6, 3, 6, 22, 44);
