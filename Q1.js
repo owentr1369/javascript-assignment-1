f05875129612e08bcef20caab3d2d450d9a71d08
owentr1369 <tamvaa1306@gmail.com>
2022-06-14 16:04:47 +0700

    completed Q1

--- Q1.js
+++ Q1.js
@@ -1,11 +1,16 @@
+const quest1 = new Object();
 function customCalc() {
   let sum = 0;
+  let avg = 0;
   for (let i = 0; i < arguments.length; i++) {
     sum += arguments[i];
+    avg = sum / arguments.length;
   }
-  console.log(sum);
-  console.log(Math.max(...arguments));
-  console.log(Math.min(...arguments));
+  quest1.max = Math.max(...arguments);
+  quest1.min = Math.min(...arguments);
+  quest1.avg = avg;
+  quest1.sum = sum;
+  console.log(quest1);
 }
 
-customCalc(4, 7, 32, 6, 3, 6, 22, 44);
+customCalc(1, 4, 5, 2, 5, 6, 7, 8, 4, 67, 232, 64);
