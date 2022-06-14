bbd2f306f2904968f0676affd8dd75ced57a1a39
owentr1369 <tamvaa1306@gmail.com>
2022-06-14 16:51:12 +0700

    completed 2a

--- Q2.js
+++ Q2.js
@@ -1,13 +1,17 @@
 const array = [1, 2, 3, 2, 5, 7, 8, 6, 6, 8, 8, 11, 52];
+const afterFilted = [];
 
 function customFilter(array, callback) {
-  console.log(array);
-  //   console.log("Hello");
-  callback(123);
+  callback(array);
 }
 
-function callbackFilter(index) {
-  console.log(index);
+function callbackFilter(array, index) {
+  for (index = 0; index < array.length; index++) {
+    if (array[index] % 2 === 0) {
+      afterFilted.push(array[index]);
+    }
+  }
+  console.log(afterFilted);
 }
 
 customFilter(array, callbackFilter);
