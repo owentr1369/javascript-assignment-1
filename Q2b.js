69e894e51315e357939ed816885fe8b4b4cd2d87
owentr1369 <tamvaa1306@gmail.com>
2022-06-15 10:51:01 +0700

    input output q2 a & b

--- Q2b.js
+++ Q2b.js
@@ -1,5 +1,5 @@
-const array = [2, 7, 33, 64, 4];
-// Output: [7, 33, 64]
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Output: [0, 2, 4, 6, 8]
 const afterFilted = [];
 
 function customFilter(array, callback) {
@@ -10,7 +10,7 @@ function customFilter(array, callback) {
 }
 
 function callbackFilter(value, index) {
-  if (value > 5) {
+  if (value % 2 === 0) {
     afterFilted.push(array[index]);
   }
 }
